28a88253c7f5a036ff86e283f3f9957a3b868451
Anykhet <anykhet@hotmail.com>
2024-06-21 21:00:33 +0200

    end

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sounds = {
-        'Space': 'sounds/sound-space.mp3',
-        'ArrowUp': 'sounds/sound-up.mp3',
-        'ArrowRight': 'sounds/sound-right.mp3',
-        'ArrowLeft': 'sounds/sound-left.mp3'
+        'Space': 'https://anykhet.github.io/Interface-Design_Endabgabe/sounds/sound-space.mp3',
+        'ArrowUp': 'https://anykhet.github.io/Interface-Design_Endabgabe/sounds/sound-up.mp3',
+        'ArrowRight': 'https://anykhet.github.io/Interface-Design_Endabgabe/sounds/sound-right.mp3',
+        'ArrowLeft': 'https://anykhet.github.io/Interface-Design_Endabgabe/sounds/sound-left.mp3'
     };
 
     const visualizer = document.getElementById('visualizer');
